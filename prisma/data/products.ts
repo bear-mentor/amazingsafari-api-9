0c7a62061fc0a1558215dc1a29f70003be899702
M Haidar Hanif <git@mhaidarhanif.com>
2025-10-14 22:05:45 +0700

    feat: seed products

---
+++ prisma/data/products.ts
@@ -0,0 +1,12 @@
+export const dataProducts = [
+  {
+    slug: "penguin-humboldt-7-inch-plush",
+    name: "Penguin Humboldt 7 Inch Plush",
+    imageUrl:
+      "https://www.safariwonders.com/wp-content/uploads/2025/06/id-11134207-7rasi-m3g30rles8d46e@resize_w450_nl.webp",
+    price: 95000,
+    stock: 10,
+    description:
+      "Fitur: 1. Kain lembut dan nyaman, isian pilihan berkualitas tinggi, tidak mudah rusak 2. Terbuat dari bahan yang aman, sehat dan ramah lingkungan, cocok untuk anak-anak dan orang dewasa 3. Bentuk lucu dengan jahit halus yang merupakan ide hadiah bagus untuk anak-anak atau orang dewasa",
+  },
+];
