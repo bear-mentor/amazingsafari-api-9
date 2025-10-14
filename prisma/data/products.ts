8165cd316d59c8b20b9b5ec15fd49ac09ffcea54
M Haidar Hanif <git@mhaidarhanif.com>
2025-10-14 23:51:07 +0700

    feat: original product links

--- prisma/data/products.ts
+++ prisma/data/products.ts
@@ -10,3 +10,12 @@ export const dataProducts = [
       "Fitur: 1. Kain lembut dan nyaman, isian pilihan berkualitas tinggi, tidak mudah rusak 2. Terbuat dari bahan yang aman, sehat dan ramah lingkungan, cocok untuk anak-anak dan orang dewasa 3. Bentuk lucu dengan jahit halus yang merupakan ide hadiah bagus untuk anak-anak atau orang dewasa",
   },
 ];
+
+/**
+ * https://www.safariwonders.com/produk/taman-safari-boneka-beruang-madu-lucu-honey-bear-plush-official-store/
+ * https://www.safariwonders.com/produk/taman-safari-boneka-koala-satwa-plush-toy-official-store/
+ * https://www.safariwonders.com/produk/taman-safari-boneka-panda-duduk-i-sitting-panda-11-5/
+ * https://www.safariwonders.com/produk/taman-safari-boneka-harimau-tiger-stuffed-plush-toy/
+ * https://www.safariwonders.com/produk/taman-safari-boneka-penguin-humboldt-7-inch-lokal-brand-plush-toys/
+ * https://www.safariwonders.com/produk/taman-safari-boneka-komodo-besar-stuff-52-lokal-brand-plush-toys/
+ */
