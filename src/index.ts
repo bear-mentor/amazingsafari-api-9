8c9f2191a399091b918653b45134c7c1e7a97fdb
M Haidar Hanif <git@mhaidarhanif.com>
2025-10-14 21:58:51 +0700

    feat: hono zod openapi

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { OpenAPIHono, createRoute, z } from "@hono/zod-openapi";
 
 import { db } from "./lib/db";
 
-const app = new Hono();
+const app = new OpenAPIHono();
 
 app.use(cors());
 
@@ -13,10 +13,45 @@ app.get("/", (c) => {
   });
 });
 
-app.get("/products", async (c) => {
+const getProductsRoute = createRoute({
+  method: "get",
+  path: "/products",
+  responses: {
+    200: {
+      description: "Get all products",
+      content: {
+        "application/json": {
+          schema: z
+            .object({
+              id: z.string(),
+              slug: z.string(),
+              name: z.string(),
+              imageUrl: z.string(),
+              price: z.number(),
+              stock: z.number(),
+              description: z.string(),
+              createdAt: z.date(),
+              updatedAt: z.date(),
+            })
+            .array(),
+        },
+      },
+    },
+  },
+});
+
+app.openapi(getProductsRoute, async (c) => {
   const products = await db.product.findMany();
 
   return c.json(products);
+});
+
+app.doc("/openapi.json", {
+  openapi: "3.0.0",
+  info: {
+    title: "Amazing Safari API",
+    version: "1.0.0",
+  },
 });
 
 export default app;
