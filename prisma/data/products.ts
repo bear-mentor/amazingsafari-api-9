016047ae7e2bc865a2565b92b74c7ae0af1415a9
M Haidar Hanif <git@mhaidarhanif.com>
2025-10-17 05:57:13 +0700

    feat: seed more products

--- prisma/data/products.ts
+++ prisma/data/products.ts
@@ -1,21 +1,82 @@
 export const dataProducts = [
   {
-    slug: "penguin-humboldt-7-inch-plush",
+    slug: "taman-safari-boneka-penguin-humboldt-7-inch-lokal-brand-plush-toys",
     name: "Penguin Humboldt 7 Inch Plush",
     imageUrl:
-      "https://www.safariwonders.com/wp-content/uploads/2025/06/id-11134207-7rasi-m3g30rles8d46e@resize_w450_nl.webp",
+      "https://safariwonders.com/wp-content/uploads/2025/06/id-11134207-7rasi-m3g30rles8d46e@resize_w450_nl.webp",
     price: 95000,
     stock: 10,
     description:
       "Fitur: 1. Kain lembut dan nyaman, isian pilihan berkualitas tinggi, tidak mudah rusak 2. Terbuat dari bahan yang aman, sehat dan ramah lingkungan, cocok untuk anak-anak dan orang dewasa 3. Bentuk lucu dengan jahit halus yang merupakan ide hadiah bagus untuk anak-anak atau orang dewasa",
   },
-];
+  // https://safariwonders.com/produk/taman-safari-boneka-penguin-humboldt-7-inch-lokal-brand-plush-toys/
 
-/**
- * https://www.safariwonders.com/produk/taman-safari-boneka-beruang-madu-lucu-honey-bear-plush-official-store/
- * https://www.safariwonders.com/produk/taman-safari-boneka-koala-satwa-plush-toy-official-store/
- * https://www.safariwonders.com/produk/taman-safari-boneka-panda-duduk-i-sitting-panda-11-5/
- * https://www.safariwonders.com/produk/taman-safari-boneka-harimau-tiger-stuffed-plush-toy/
- * https://www.safariwonders.com/produk/taman-safari-boneka-penguin-humboldt-7-inch-lokal-brand-plush-toys/
- * https://www.safariwonders.com/produk/taman-safari-boneka-komodo-besar-stuff-52-lokal-brand-plush-toys/
- */
+  {
+    slug: "taman-safari-boneka-beruang-madu-lucu-honey-bear-plush-official-store",
+    name: "Boneka Beruang Madu Honey Bear Plush",
+    imageUrl:
+      "https://safariwonders.com/wp-content/uploads/2025/06/id-11134207-7r98u-ly1qsdaikbre3b@resize_w450_nl.webp",
+    price: 79000,
+    stock: 10,
+    description:
+      "Fitur: 1. Kain lembut dan nyaman, isian pilihan berkualitas tinggi, tidak mudah rusak 2. Terbuat dari bahan yang aman, sehat dan ramah lingkungan, cocok untuk anak-anak dan orang dewasa 3. Bentuk lucu dengan jahit halus yang merupakan ide hadiah bagus untuk anak-anak atau orang dewasa. Berat: 100 gram",
+  },
+  // https://safariwonders.com/produk/taman-safari-boneka-beruang-madu-lucu-honey-bear-plush-official-store/
+
+  {
+    slug: "taman-safari-boneka-koala-satwa-plush-toy-official-store",
+    name: "Boneka Koala Satwa Plush",
+    imageUrl:
+      "https://safariwonders.com/wp-content/uploads/2025/06/id-11134207-7r992-ly1qsdb2dwh809@resize_w450_nl.webp",
+    price: 65000,
+    stock: 10,
+    description:
+      "Fitur: 1. Kain lembut dan nyaman, isian pilihan berkualitas tinggi, tidak mudah rusak 2. Terbuat dari bahan yang aman, sehat dan ramah lingkungan, cocok untuk anak-anak dan orang dewasa 3. Bentuk lucu dengan jahit halus yang merupakan ide hadiah bagus untuk anak-anak atau orang dewasa. Ukuran: 14 x 9 x 5 cm. Berat: 75 gram",
+  },
+  // https://safariwonders.com/produk/taman-safari-boneka-koala-satwa-plush-toy-official-store/
+
+  {
+    slug: "taman-safari-boneka-panda-duduk-i-sitting-panda-11-5",
+    name: "Boneka Panda Duduk Sitting Panda 11.5",
+    imageUrl:
+      "https://safariwonders.com/wp-content/uploads/2025/06/id-11134207-7r98r-ly23q8yezpke73@resize_w450_nl.webp",
+    price: 215000,
+    stock: 10,
+    description:
+      "Fitur: 1. Kain lembut dan nyaman, isian pilihan berkualitas tinggi, tidak mudah rusak 2. Terbuat dari bahan yang aman, sehat dan ramah lingkungan, cocok untuk anak-anak dan orang dewasa 3. Bentuk lucu dengan jahit halus yang merupakan ide hadiah bagus untuk anak-anak atau orang dewasa",
+  },
+  // https://safariwonders.com/produk/taman-safari-boneka-panda-duduk-i-sitting-panda-11-5/
+
+  {
+    slug: "taman-safari-boneka-harimau-tiger-stuffed-plush-toy",
+    name: "Boneka Harimau Tiger Stuffed Plush",
+    imageUrl:
+      "https://safariwonders.com/wp-content/uploads/2025/06/id-11134207-7r98r-ly1qsdb2kxbgd0@resize_w450_nl.webp",
+    price: 89000,
+    stock: 10,
+    description:
+      "Fitur: 1. Kain lembut dan nyaman, isian pilihan berkualitas tinggi, tidak mudah rusak 2. Terbuat dari bahan yang aman, sehat dan ramah lingkungan, cocok untuk anak-anak dan orang dewasa 3. Bentuk lucu dengan jahit halus yang merupakan ide hadiah bagus untuk anak-anak atau orang dewasa. Ukuran : 23 x 6 x 12 cm. Berat: 70 gram.",
+  },
+  // https://safariwonders.com/produk/taman-safari-boneka-harimau-tiger-stuffed-plush-toy/
+
+  {
+    slug: "taman-safari-boneka-komodo-besar-stuff-52-lokal-brand-plush-toys",
+    name: "Boneka Komodo Besar Stuff 52″ Lokal Brand Plush",
+    imageUrl:
+      "https://safariwonders.com/wp-content/uploads/2025/06/id-11134207-7ras9-m5xpxwh3z4r76c@resize_w450_nl.webp",
+    price: 460000,
+    stock: 10,
+    description:
+      "Fitur: 1. Kain lembut dan nyaman, isian pilihan berkualitas tinggi, tidak mudah rusak 2. Terbuat dari bahan yang aman, sehat dan ramah lingkungan, cocok untuk anak-anak dan orang dewasa 3. Bentuk lucu dengan jahit halus yang merupakan ide hadiah bagus untuk anak-anak atau orang dewasa. Berat: 2 KG",
+  },
+  // https://safariwonders.com/produk/taman-safari-boneka-komodo-besar-stuff-52-lokal-brand-plush-toys
+
+  // {
+  //   slug: "",
+  //   name: "",
+  //   imageUrl: "",
+  //   price: 0,
+  //   stock: 10,
+  //   description: "",
+  // },
+];
