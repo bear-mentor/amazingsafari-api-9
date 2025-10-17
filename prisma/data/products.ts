0fa7f5150c051f8cd7181546a58e139957bf0400
M Haidar Hanif <git@mhaidarhanif.com>
2025-10-17 22:15:25 +0700

    refactor: change seed products image url

--- prisma/data/products.ts
+++ prisma/data/products.ts
@@ -1,9 +1,9 @@
 export const dataProducts = [
   {
-    slug: "taman-safari-boneka-penguin-humboldt-7-inch-lokal-brand-plush-toys",
+    slug: "penguin-humboldt-7-inch-plush",
     name: "Penguin Humboldt 7 Inch Plush",
     imageUrl:
-      "https://safariwonders.com/wp-content/uploads/2025/06/id-11134207-7rasi-m3g30rles8d46e@resize_w450_nl.webp",
+      "https://68wwbouzac.ucarecd.net/0c48558f-dd8a-48df-a63c-73b2d5705ab8/-/preview/450x450/",
     price: 95000,
     stock: 10,
     description:
@@ -12,10 +12,10 @@ export const dataProducts = [
   // https://safariwonders.com/produk/taman-safari-boneka-penguin-humboldt-7-inch-lokal-brand-plush-toys/
 
   {
-    slug: "taman-safari-boneka-beruang-madu-lucu-honey-bear-plush-official-store",
+    slug: "boneka-beruang-madu-honey-bear-plush",
     name: "Boneka Beruang Madu Honey Bear Plush",
     imageUrl:
-      "https://safariwonders.com/wp-content/uploads/2025/06/id-11134207-7r98u-ly1qsdaikbre3b@resize_w450_nl.webp",
+      "https://68wwbouzac.ucarecd.net/f37c1ca5-3cae-428d-9be5-6ecd39dd33da/-/preview/450x450/",
     price: 79000,
     stock: 10,
     description:
@@ -24,10 +24,10 @@ export const dataProducts = [
   // https://safariwonders.com/produk/taman-safari-boneka-beruang-madu-lucu-honey-bear-plush-official-store/
 
   {
-    slug: "taman-safari-boneka-koala-satwa-plush-toy-official-store",
+    slug: "boneka-koala-satwa-plush",
     name: "Boneka Koala Satwa Plush",
     imageUrl:
-      "https://safariwonders.com/wp-content/uploads/2025/06/id-11134207-7r992-ly1qsdb2dwh809@resize_w450_nl.webp",
+      "https://68wwbouzac.ucarecd.net/03dab25d-fe77-4ddc-9b0b-9d7203e85572/-/preview/450x450/",
     price: 65000,
     stock: 10,
     description:
@@ -36,10 +36,10 @@ export const dataProducts = [
   // https://safariwonders.com/produk/taman-safari-boneka-koala-satwa-plush-toy-official-store/
 
   {
-    slug: "taman-safari-boneka-panda-duduk-i-sitting-panda-11-5",
+    slug: "boneka-panda-duduk-sitting-panda-11-5",
     name: "Boneka Panda Duduk Sitting Panda 11.5",
     imageUrl:
-      "https://safariwonders.com/wp-content/uploads/2025/06/id-11134207-7r98r-ly23q8yezpke73@resize_w450_nl.webp",
+      "https://68wwbouzac.ucarecd.net/7fcb8e96-a0ef-416c-af73-651794fff521/-/preview/450x450/",
     price: 215000,
     stock: 10,
     description:
@@ -48,10 +48,10 @@ export const dataProducts = [
   // https://safariwonders.com/produk/taman-safari-boneka-panda-duduk-i-sitting-panda-11-5/
 
   {
-    slug: "taman-safari-boneka-harimau-tiger-stuffed-plush-toy",
+    slug: "boneka-harimau-tiger-stuffed-plush",
     name: "Boneka Harimau Tiger Stuffed Plush",
     imageUrl:
-      "https://safariwonders.com/wp-content/uploads/2025/06/id-11134207-7r98r-ly1qsdb2kxbgd0@resize_w450_nl.webp",
+      "https://68wwbouzac.ucarecd.net/9a9ad48a-dda0-40b2-930b-f9628f77b57e/-/preview/450x450/",
     price: 89000,
     stock: 10,
     description:
@@ -60,10 +60,10 @@ export const dataProducts = [
   // https://safariwonders.com/produk/taman-safari-boneka-harimau-tiger-stuffed-plush-toy/
 
   {
-    slug: "taman-safari-boneka-komodo-besar-stuff-52-lokal-brand-plush-toys",
+    slug: "boneka-komodo-besar-stuff-52-lokal-brand-plush",
     name: "Boneka Komodo Besar Stuff 52″ Lokal Brand Plush",
     imageUrl:
-      "https://safariwonders.com/wp-content/uploads/2025/06/id-11134207-7ras9-m5xpxwh3z4r76c@resize_w450_nl.webp",
+      "https://68wwbouzac.ucarecd.net/66261cb3-1b2c-41af-b44b-73f10018c2dc/-/preview/450x450/",
     price: 460000,
     stock: 10,
     description:
