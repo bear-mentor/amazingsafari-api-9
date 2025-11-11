3d5cf589b545a0a19cb544dafd28c77015a66aa1
M Haidar Hanif <git@mhaidarhanif.com>
2025-11-11 22:02:04 +0700

    refactor: move auth route to module

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,7 @@ import { signToken } from "./lib/token";
 import { checkAuthorized } from "./modules/auth/middleware";
 import { productRoute } from "./modules/product/route";
 import { userRoute } from "./modules/user/route";
+import { authRoute } from "./modules/auth/route";
 
 const app = new OpenAPIHono();
 
@@ -28,128 +29,7 @@ app.use(cors());
 
 app.route("/products", productRoute);
 app.route("/users", userRoute);
-
-// POST /auth/register
-app.openapi(
-  createRoute({
-    method: "post",
-    path: "/auth/register",
-    request: {
-      body: { content: { "application/json": { schema: RegisterUserSchema } } },
-    },
-    responses: {
-      201: {
-        description: "Registered new user",
-        content: { "application/json": { schema: UserSchema } },
-      },
-      400: {
-        description: "Failed to register new user",
-      },
-    },
-  }),
-  async (c) => {
-    const body = c.req.valid("json");
-
-    try {
-      const hash = await Bun.password.hash(body.password);
-
-      const user = await db.user.create({
-        data: {
-          username: body.username,
-          email: body.email,
-          fullName: body.fullName,
-          password: { create: { hash } },
-        },
-      });
-
-      return c.json(user, 201);
-    } catch (error) {
-      return c.json({ message: "Username or email already exist" }, 400);
-    }
-  }
-);
-
-// POST /auth/login
-app.openapi(
-  createRoute({
-    method: "post",
-    path: "/auth/login",
-    request: {
-      body: { content: { "application/json": { schema: LoginUserSchema } } },
-    },
-    responses: {
-      200: {
-        description: "Logged in to user",
-        content: { "text/plain": { schema: TokenSchema } },
-      },
-      400: {
-        description: "Failed to login user",
-      },
-      404: {
-        description: "User not found",
-      },
-    },
-  }),
-  async (c) => {
-    const body = c.req.valid("json");
-
-    try {
-      const user = await db.user.findUnique({
-        where: { email: body.email },
-        include: {
-          password: true,
-        },
-      });
-
-      if (!user) {
-        return c.notFound();
-      }
-
-      if (!user.password?.hash) {
-        return c.json({
-          message: "User has no password",
-        });
-      }
-
-      const isMatch = await Bun.password.verify(
-        body.password,
-        user.password.hash
-      );
-
-      if (!isMatch) {
-        return c.json({
-          message: "Password incorrect",
-        });
-      }
-
-      const token = await signToken(user.id);
-
-      return c.text(token);
-    } catch (error) {
-      return c.json({ message: "Email or password is incorrect" }, 400);
-    }
-  }
-);
-
-// GET /auth/me
-app.openapi(
-  createRoute({
-    method: "get",
-    path: "/auth/me",
-    middleware: checkAuthorized,
-    responses: {
-      200: {
-        description: "Get authenticated user",
-        content: { "application/json": { schema: PrivateUserSchema } },
-      },
-    },
-  }),
-  async (c) => {
-    const user = c.get("user");
-
-    return c.json(user);
-  }
-);
+app.route("/auth", authRoute);
 
 app.doc("/openapi.json", {
   openapi: "3.0.0",
